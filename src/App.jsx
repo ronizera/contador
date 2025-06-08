7d95c1a86a9e894315e391a934334256ec021fbd
ronizera <roniltonbreno@gmail.com>
2025-06-08 04:35:49 -0300

    tirando o icon

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,12 @@ function App() {
       <div className='bg-white shadow-lg rounded-xl p-8 text-center'>
 
         <h1 className='text-2xl font-bold mb-4'>Contador: {contador}</h1>
-        <div className='flex gap-8'>
-          <button className='bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 ' onClick={() => setContador(contador + 1)}>Aumentar</button>
-          <button className='bg-red-500 text-white px-4 py-2 rounded hover:bg-red-800 ' onClick={() => setContador(contador - 1)}>Diminuir</button>
-          <button className='bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600' onClick={() => setContador(0)}>Resetar</button>
+
+        <div className='flex gap-4'>
+
+            <button className='bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 ' onClick={() => setContador(contador + 1)}>Aumentar</button>
+            <button className='bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 ' onClick={() => setContador(contador - 1)}>Diminuir</button>
+            <button className='bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600' onClick={() => setContador(0)}>Resetar</button>
         </div>
       </div>
     </div>
